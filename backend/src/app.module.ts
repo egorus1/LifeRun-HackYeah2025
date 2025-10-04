36c49717c0ad6715347fd06a53866b932230cd07
Yehor Pishyi <egorpeshiy@gmail.com>
2025-10-04 18:17:24 +0200

    Modules Created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuestionsModule } from './questions/questions.module';
+import { UsersModule } from './users/users.module';
+import { AnalysisModule } from './analysis/analysis.module';
 
 @Module({
-  imports: [],
+  imports: [QuestionsModule, UsersModule, AnalysisModule],
   controllers: [AppController],
   providers: [AppService],
 })
